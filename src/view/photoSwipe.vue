be90ebd82eb18a16874946aec4fe925b7b88ab9d
huangjia <5263954@qq.com>
2018-12-07 14:14:49 +0800

    delete and update

--- src/view/photoSwipe.vue
+++ src/view/photoSwipe.vue
@@ -1,71 +1,76 @@
 <template>
-    <div class="photoSwipe">
-      <simple-photoswipe :slides="slides" :options="options" @close="close" @afterChange="afterChange" @beforeChange="beforeChange" @destroy="destroy" ref="simplePhotoswipe"></simple-photoswipe>
-    </div>
+  <div class="photoSwipe">
+    <simple-photoswipe :slides="slides"
+                       :options="options"
+                       @close="close"
+                       @afterChange="afterChange"
+                       @beforeChange="beforeChange"
+                       @destroy="destroy"
+                       ref="simplePhotoswipe"></simple-photoswipe>
+  </div>
 </template>
 <script>
-/* eslint-disable */
 import { simplePhotoswipe } from 'simple-photoswipe'
-export default{
-    data:function(){
-        return {
-            slides:[
-                {
-                    src: 'https://img.zcool.cn/community/01ff2059770a25a8012193a37c7695.jpg@1280w_1l_2o_100sh.jpg',
-                    msrc:'https://img.zcool.cn/community/01ff2059770a25a8012193a37c7695.jpg@1280w_1l_2o_100sh.jpg',
-                    w: 964,
-                    h: 1024
-                },
-                {
-                    src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541502380130&di=9126f8939434001d6536c9d32bacffcb&imgtype=0&src=http%3A%2F%2Ftvax4.sinaimg.cn%2Fcrop.0.0.996.996.1024%2F006HxkHbly8flgt44j5rij30ro0rpacl.jpg',
-                    msrc: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541502380130&di=9126f8939434001d6536c9d32bacffcb&imgtype=0&src=http%3A%2F%2Ftvax4.sinaimg.cn%2Fcrop.0.0.996.996.1024%2F006HxkHbly8flgt44j5rij30ro0rpacl.jpg',
-                    w: 964,
-                    h: 1024
-                }
-            ],
-            options:{
-                index:0,
-                shareEl: false
-            }
+export default {
+  data: function () {
+    return {
+      slides: [
+        {
+          src: 'https://img.zcool.cn/community/01ff2059770a25a8012193a37c7695.jpg@1280w_1l_2o_100sh.jpg',
+          msrc: 'https://img.zcool.cn/community/01ff2059770a25a8012193a37c7695.jpg@1280w_1l_2o_100sh.jpg',
+          w: 964,
+          h: 1024
+        },
+        {
+          src: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541502380130&di=9126f8939434001d6536c9d32bacffcb&imgtype=0&src=http%3A%2F%2Ftvax4.sinaimg.cn%2Fcrop.0.0.996.996.1024%2F006HxkHbly8flgt44j5rij30ro0rpacl.jpg',
+          msrc: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1541502380130&di=9126f8939434001d6536c9d32bacffcb&imgtype=0&src=http%3A%2F%2Ftvax4.sinaimg.cn%2Fcrop.0.0.996.996.1024%2F006HxkHbly8flgt44j5rij30ro0rpacl.jpg',
+          w: 964,
+          h: 1024
         }
-    },
-    components:{
-       simplePhotoswipe
-    },
-    methods:{
-        close(){
-            console.log('close')
-        },
-        afterChange(){
-            console.log('afterChange')
-        },
-        beforeChange(){
-            console.log('beforeChange')
-        },
-        goTo(index){
-            console.log('goTo'+index)
-        },
-        destroy(){
-            console.log('destroy')
-        }
-        
+      ],
+      options: {
+        index: 0,
+        shareEl: false
+      }
     }
+  },
+  components: {
+    simplePhotoswipe
+  },
+  methods: {
+    close () {
+      console.log('close')
+    },
+    afterChange () {
+      console.log('afterChange')
+    },
+    beforeChange () {
+      console.log('beforeChange')
+    },
+    goTo (index) {
+      console.log('goTo' + index)
+    },
+    destroy () {
+      console.log('destroy')
+    }
+
+  }
 }
 </script>
 <style scoped lang="scss">
-#app {
-    height: 100%;
-}
-.photoSwipe{
-    height: 100%;
-    /deep/{
-        figure{
-            float: left;
-            margin: 0 10px 0 0;
-            padding: 0;
-            width: 500px;
-            height: 300px;
-        }
+.photoSwipe {
+  /deep/ {
+    figure {
+      float: left;
+      margin: 0 10px 0 0;
+      padding: 0;
+      width: 300px;
+      height: 300px;
     }
+    img {
+      width: 300px;
+      height: 300px;
+    }
+  }
 }
 </style>
