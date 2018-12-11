8e603961f4e4ed38f273c5b1318289d384a61819
huangjia <5263954@qq.com>
2018-12-11 15:58:02 +0800

    delete

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// 主页面结构
 const photoSwipe = r => require.ensure([], () => r(require('@/view/photoSwipe.vue')), 'photoSwipe')
 
 Vue.use(Router)
-const routes = [
-  {
-    path: '*',
-    redirect: '/photoSwipe'
-  },
-  {
-    path: '/photoSwipe',
-    name: 'photoSwipe',
-    component: photoSwipe
-  }
+const routes = [{
+  path: '*',
+  redirect: '/photoSwipe'
+},
+{
+  path: '/photoSwipe',
+  name: 'photoSwipe',
+  component: photoSwipe
+}
 ]
 
 const router = new Router({
@@ -24,12 +22,13 @@ const router = new Router({
     if (savedPosition) {
       return savedPosition
     } else {
-      return {x: 0, y: 0}
+      return {
+        x: 0,
+        y: 0
+      }
     }
   }
 })
-// 全局路由配置
-// 路由开始之前的操作
 router.beforeEach((to, from, next) => {
   next()
 })
